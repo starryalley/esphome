6c22e093ceeb47c3c9870995cdf9b32d940a986e
WeekendWarrior1 <c.halton.93@gmail.com>
2021-06-21 20:23:11 +1000

    not working - ESPhome doesn't support dynamically swapping CONFIG_SCHEMA extends

--- esphome/components/rgb/light.py
+++ esphome/components/rgb/light.py
@@ -6,18 +6,48 @@ from esphome.const import CONF_BLUE, CONF_GREEN, CONF_RED, CONF_OUTPUT_ID
 rgb_ns = cg.esphome_ns.namespace("rgb")
 RGBLightOutput = rgb_ns.class_("RGBLightOutput", light.LightOutput)
 
-CONFIG_SCHEMA = light.RGB_LIGHT_SCHEMA.extend(
-    {
-        cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(RGBLightOutput),
-        cv.Required(CONF_RED): cv.use_id(output.FloatOutput),
-        cv.Required(CONF_GREEN): cv.use_id(output.FloatOutput),
-        cv.Required(CONF_BLUE): cv.use_id(output.FloatOutput),
-    }
+RGBLightOutputAddressable = rgb_ns.class_(
+    "RGBLightOutputAddressable", light.AddressableLight
 )
+
+CONF_ADDRESSABLE_PIXEL = "addressable_pixel"
+
+if config[CONF_ADDRESSABLE_PIXEL]:
+    CONFIG_SCHEMA = light.RGB_LIGHT_SCHEMA.extend(
+        {
+            cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(RGBLightOutput),
+            cv.Required(CONF_RED): cv.use_id(output.FloatOutput),
+            cv.Required(CONF_GREEN): cv.use_id(output.FloatOutput),
+            cv.Required(CONF_BLUE): cv.use_id(output.FloatOutput),
+            cv.Optional(CONF_ADDRESSABLE_PIXEL, default=False): cv.boolean,
+        }
+    )
+else:
+    CONFIG_SCHEMA = light.ADDRESSABLE_LIGHT_SCHEMA.extend(
+        {
+            cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(RGBLightOutput),
+            cv.Required(CONF_RED): cv.use_id(output.FloatOutput),
+            cv.Required(CONF_GREEN): cv.use_id(output.FloatOutput),
+            cv.Required(CONF_BLUE): cv.use_id(output.FloatOutput),
+            cv.Optional(CONF_ADDRESSABLE_PIXEL, default=False): cv.boolean,
+        }
+    )
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
+    #template = cg.TemplateArguments(getattr(cg.global_ns, format_method(config)))
+    if config[CONF_ADDRESSABLE_PIXEL]:
+        #out_type = NeoPixelRGBWLightOutput.template(template)
+        out_type = RGBLightOutputAddressable
+    else:
+        #out_type = NeoPixelRGBLightOutput.template(template)
+        out_type = RGBLightOutput
+
+
+    rhs = out_type.new()
+    var = cg.Pvariable(config[CONF_OUTPUT_ID], rhs, out_type)
+
+    #var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
     await light.register_light(var, config)
 
     red = await cg.get_variable(config[CONF_RED])
--- esphome/components/rgb/rgb_light_output.h
+++ esphome/components/rgb/rgb_light_output.h
@@ -4,8 +4,15 @@
 #include "esphome/components/output/float_output.h"
 #include "esphome/components/light/light_output.h"
 
+#include "esphome/core/color.h"
+#include "esphome/components/light/addressable_light.h"
+
+
 namespace esphome {
 namespace rgb {
+
+static const char *const TAG = "rgb";
+
 
 class RGBLightOutput : public light::LightOutput {
  public:
@@ -19,6 +26,8 @@ class RGBLightOutput : public light::LightOutput {
     traits.set_supports_rgb(true);
     return traits;
   }
+
+
   void write_state(light::LightState *state) override {
     float red, green, blue;
     state->current_values_as_rgb(&red, &green, &blue, false);
@@ -32,6 +41,54 @@ class RGBLightOutput : public light::LightOutput {
   output::FloatOutput *green_;
   output::FloatOutput *blue_;
 };
+
+
+
+
+class RGBLightOutputAddressable : public light::AddressableLight {
+ public:
+  light::LightTraits get_traits() override {
+    auto traits = light::LightTraits();
+    traits.set_supports_brightness(true);
+    traits.set_supports_rgb(true);
+    return traits;
+  }
+
+  void setup() override {
+    for (int i = 0; i < this->size(); i++) {
+      //(*this)[i] = Color(0, 0, 0, 0);
+      /*this->red_int_ = 0;
+      this->green_int_ = 0;
+      this->blue_int_ = 0;*/
+
+      /*auto call = this->state_->make_call();
+      call.set_color_temperature(this->cold_white_temperature_ +
+                                 (this->warm_white_temperature_ - this->cold_white_temperature_) *
+                                     (float(datapoint.value_uint) / float(this->color_temperature_max_value_)));
+      call.perform();*/
+
+      //hardwarePWMobj.write_state()  (this->red_int_, this->green_int_, this->blue_int_);
+    }
+  }
+
+  int32_t size() const override { return 1; }
+
+  void clear_effect_data() override {
+    for (int i = 0; i < this->size(); i++)
+      this->effect_data_[i] = 0;
+  }
+
+ protected:
+  uint8_t red_int_;
+  uint8_t green_int_;
+  uint8_t blue_int_;
+  RGBLightOutput hardwarePWMobj;
+  light::LightState *state_{nullptr};
+  uint8_t *effect_data_{nullptr};
+
+};
+
+
 
 }  // namespace rgb
 }  // namespace esphome
