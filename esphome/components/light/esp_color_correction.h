987fbaaf6b03e325baf36ea39d540447870a1b88
WeekendWarrior1 <c.halton.93@gmail.com>
2021-07-20 21:58:09 +1000

    fix white channel in RGBW

--- esphome/components/light/esp_color_correction.h
+++ esphome/components/light/esp_color_correction.h
@@ -29,7 +29,8 @@ class ESPColorCorrection {
     return this->gamma_table_[res];
   }
   inline uint8_t color_correct_white(uint8_t white) const ALWAYS_INLINE {
-    uint8_t res = esp_scale8(esp_scale8(white, this->max_brightness_.white), this->local_brightness_);
+    //uint8_t res = esp_scale8(esp_scale8(white, this->max_brightness_.white), this->local_brightness_);
+    uint8_t res = esp_scale8(white, this->max_brightness_.white);
     return this->gamma_table_[res];
   }
   inline Color color_uncorrect(Color color) const ALWAYS_INLINE {
